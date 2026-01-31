0d65bb8105be5c188a728edf4aa1a01b313f9c7e
ikawamuk <ikawamuk@student.42.fr>
2026-01-31 15:57:27 +0900

    update:

--- fibonacci/src/main.rs
+++ fibonacci/src/main.rs
@@ -1,23 +1,45 @@
-use std::env;
+use std::io;
 
 fn main() {
-	hello_world("Takeshi");
-	fibonacci(10);
+	hello_world("fibonacci");
+	let input: i32 = loop {
+	let mut input: String = String::new();
+		io::stdin().read_line(&mut input).expect("Failed to read");
+		match input.trim().parse() {
+			Ok(n)=> {break n;}
+			Err(_)=>{
+				println!("Input a number");
+				continue ;
+			}
+		};
+	};
+	fibonacci(input);
 }
 
 fn fibonacci(n: i32){
 	let mut a: i32 = 1;
 	let mut b: i32 = 1;
-	let mut sum: i32;
+	let mut sum: i32 = 1;
 
-	if n >= 1 {println!("{}", a);}
-	if n >= 2 {println!("{}", b);}
+	if n == 0 {
+		println!("0 is invalid");
+		return ;
+	}
+	if n == 1 {
+		println!("{}", a);
+		return ;
+	}
+	if n == 2 {
+		println!("{}", b);
+		return ;
+	}
 	for _i in 2..n {
 		sum = add(a, b);
-		println!("{}",sum);
+		// println!("{}",sum);
 		a = b;
 		b = sum;
 	}
+	println!("result: {}", sum);
 }
 
 fn add(a: i32, b: i32) -> i32 {
